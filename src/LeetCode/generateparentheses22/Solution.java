9e04b67a0e7f4d3a8512decbc65005bfa1ca7525
taehee-kim-dev <taehee.kim.dev@gmail.com>
2020-12-21 16:22:15 +0900

    2020.12.21

---
+++ src/LeetCode/generateparentheses22/Solution.java
@@ -0,0 +1,31 @@
+package LeetCode.generateparentheses22;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public List<String> generateParenthesis(int n) {
+        List<String> result = new ArrayList<>();
+        recursive(n, 0, 0, new StringBuilder(), result);
+        return result;
+    }
+
+    private void recursive(int n, int left, int right, StringBuilder sb, List<String> result) {
+        if (left == n && right == n) {
+            result.add(sb.toString());
+            return;
+        }
+
+        if (left < n) {
+            sb.append("(");
+            recursive(n, left + 1, right, sb, result);
+            sb.deleteCharAt(sb.length() - 1);
+        }
+
+        if (left > right) {
+            sb.append(")");
+            recursive(n, left, right + 1, sb, result);
+            sb.deleteCharAt(sb.length() - 1);
+        }
+    }
+}
